99c7e1a76e6220fb4de54090648bd3fb27391860
sochi419 <equipment2241@gmail.com>
2023-04-20 14:13:34 +0900

    命名を適切化した

--- memo_spa/src/App.vue
+++ memo_spa/src/App.vue
@@ -2,45 +2,45 @@
 export default {
   data() {
     return {
-      storageKey: 'todoList',
-      newTodo: '',
-      todoList: []
+      storageKey: 'memoList',
+      newMemo: '',
+      memoList: []
     }
   },
   methods: {
     add() {
-      if (this.newTodo.trim() === '') {
+      if (this.newMemo.trim() === '') {
         return
       }
-      for (let i = 0; i < this.todoList.length; i++) {
-        this.todoList[i].done = false
+      for (let i = 0; i < this.memoList.length; i++) {
+        this.memoList[i].done = false
       }
-      this.todoList.push({ text: this.newTodo, editText: '' })
-      localStorage.setItem(this.storageKey, JSON.stringify(this.todoList))
-      this.newTodo = ''
+      this.memoList.push({ text: this.newMemo, editText: '' })
+      localStorage.setItem(this.storageKey, JSON.stringify(this.memoList))
+      this.newMemo = ''
     },
     remove(index) {
-      this.todoList.splice(index, 1)
-      localStorage.setItem(this.storageKey, JSON.stringify(this.todoList))
+      this.memoList.splice(index, 1)
+      localStorage.setItem(this.storageKey, JSON.stringify(this.memoList))
     },
     update(index) {
-      const todo = this.todoList[index]
-      if (todo.editText !== '') {
-        todo.text = todo.editText
-        todo.editText = ''
-        localStorage.setItem(this.storageKey, JSON.stringify(this.todoList))
+      const memo = this.memoList[index]
+      if (memo.editText !== '') {
+        memo.text = memo.editText
+        memo.editText = ''
+        localStorage.setItem(this.storageKey, JSON.stringify(this.memoList))
       }
     },
-    setEditTodoValue(todo, index) {
-      this.todoList[index].editText = todo.text
+    setEditMemoValue(memo, index) {
+      this.memoList[index].editText = memo.text
     }
   },
   created() {
     const dataStr = localStorage.getItem(this.storageKey)
     if (dataStr) {
-      this.todoList = JSON.parse(dataStr)
-      for (let i = 0; i < this.todoList.length; i++) {
-        this.todoList[i].done = false
+      this.memoList = JSON.parse(dataStr)
+      for (let i = 0; i < this.memoList.length; i++) {
+        this.memoList[i].done = false
       }
     }
   }
@@ -55,20 +55,20 @@ export default {
         name="message"
         cols="30"
         rows="7"
-        v-model="newTodo"
+        v-model="newMemo"
         placeholder="memo内容を入力してください "
       ></textarea>
       <button @click="add">追加</button>
 
       <ul>
-        <li v-for="(todo, index) in todoList" :key="index">
+        <li v-for="(memo, index) in memoList" :key="index">
           <label>
-            <input type="checkbox" v-model="todo.done" @change="setEditTodoValue(todo, index)" />
-            <span @click="todo.done = !todo.done">{{ todo.text.split('\n')[0] }}</span>
+            <input type="checkbox" v-model="memo.done" @change="setEditMemoValue(memo, index)" />
+            <span @click="memo.done = !memo.done">{{ memo.text.split('\n')[0] }}</span>
           </label>
-          <input type="text" v-show="todo.done" v-model="todo.editText" />
-          <button @click="update(index)" v-show="todo.done">編集</button>
-          <button @click="remove(index)" v-show="todo.done">削除</button>
+          <input type="text" v-show="memo.done" v-model="memo.editText" />
+          <button @click="update(index)" v-show="memo.done">編集</button>
+          <button @click="remove(index)" v-show="memo.done">削除</button>
         </li>
       </ul>
     </div>
